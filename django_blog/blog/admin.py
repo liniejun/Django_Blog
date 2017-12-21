53ef0de8962f743f7b1fee0bbd051a5f79d42ae3
Leeziyou <18219529882@163.com>
2017-12-21 22:56:24 +0800

    make migrations and migrate

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_blog.blog.models import Category, Tag, Post
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# 创建分类和标签
+c = Category(name='python')
+c.save()
+t = Tag(name='数据库')
+t.save()
+
+# 新建一篇文章
+user = User.objects.get(username='admin')
+c = Category.objects.get(name='python')
+p =Post(title='我爱django', content='保持学习的热情很重要啊', create_time=timezone.now(), modified_time=timezone.now(), category=c, author=user)
+p.save()
+
+Post.objects.all()
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
 
 # Create your models here.
+# python_2_unicode_compatible 用于兼容python2
+@python_2_unicode_compatible
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    content = models.TextField()
+    create_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    abstract = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
