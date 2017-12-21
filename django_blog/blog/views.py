263b783940ab46a592f9a76f503e630906184173
Leeziyou <18219529882@163.com>
2017-12-21 20:50:10 +0800

    templates render views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+def helloworld(request):
+    context = {'current_time': datetime.now()}
+    return render(request, 'blog/helloworld.html', context)
