b01f22823907f826e7e4391f4b4c6b57163db804
Leeziyou <18219529882@163.com>
2017-12-22 19:38:52 +0800

    markdown and code highlight supported

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import markdown
+
 # Create your views here.
 def index(request):
     #  文章按照创建时间倒序排列，即最新的排在最前面
@@ -8,4 +10,8 @@ def index(request):
 
 def detail(request, id):
     post = get_object_or_404(Post, pk=id)
+    post.content = markdown.markdown(post.content, extensions=['markdown.extensions.extra',
+                                                               'markdown.extensions.codehilite',
+                                                               'markdown.extensions.toc',])
     return render(request, 'blog/detail.html', context={'post': post})
+
