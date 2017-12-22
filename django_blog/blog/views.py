bb48fdd3cb6fabafa38632524505e7f418462480
Leeziyou <18219529882@163.com>
2017-12-22 20:39:22 +0800

    category and archives supported

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 import markdown
 
 # Create your views here.
@@ -14,4 +14,14 @@ def detail(request, id):
                                                                'markdown.extensions.codehilite',
                                                                'markdown.extensions.toc',])
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(create_time__year=year, create_time__month=month).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=category).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
 
